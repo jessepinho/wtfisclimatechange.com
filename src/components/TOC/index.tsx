ccad8fe9892597c4f96b0e1f4d669935a8bc6eab
Jesse Pinho <jesse@jessepinho.com>
2019-12-28 16:10:00 +0100

    feat: continue building out the TOC

--- src/components/TOC/index.tsx
+++ src/components/TOC/index.tsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 import * as Styles from './styles'
 
-const TOC: React.FC<{}> = props => <Styles.Root>hello</Styles.Root>
+const TOC: React.FC<{}> = props => (
+  <Styles.Root>
+    <Styles.H2>We've got your back.</Styles.H2>
+    <Styles.Ul>
+      <Styles.Li>How do we know it's real?</Styles.Li>
+      <Styles.Li>What’s the devestation per degree of warming?</Styles.Li>
+      <Styles.Li>
+        What does it actually mean that sea levels are rising?
+      </Styles.Li>
+    </Styles.Ul>
+  </Styles.Root>
+)
 
 export default TOC
--- src/components/TOC/styles.ts
+++ src/components/TOC/styles.ts
@@ -1,5 +1,17 @@
 import styled from 'styled-components'
 
+import { FontFamily } from '../../lib/theme'
+
 export const Root = styled.div`
   background-color: rgb(65, 137, 222);
+  color: white;
 `
+
+export const H2 = styled.h2`
+  font-family: ${FontFamily.Heading};
+  font-size: 4rem;
+`
+
+export const Ul = styled.ul``
+
+export const Li = styled.li``
