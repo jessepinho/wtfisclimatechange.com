e50f148d126c8e027a740fb7073f27f558097775
Jesse Pinho <jesse@jessepinho.com>
2019-12-29 13:41:40 +0100

    feat: add animation to the axes

--- src/components/TemperatureOverTime/Axes/index.tsx
+++ src/components/TemperatureOverTime/Axes/index.tsx
@@ -1,30 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useScrollPercentage } from 'react-scroll-percentage'
 
 import * as Styles from './styles'
 
 const YTick: React.FC<{ degrees: number }> = ({ degrees }) => (
-  <span>
+  <Styles.YTick>
     <Styles.DegreesNumber>
       {degrees > 0 ? `+${degrees}` : degrees}
     </Styles.DegreesNumber>
     &deg;C
-  </span>
+    <Styles.Dash> &ndash;</Styles.Dash>
+  </Styles.YTick>
 )
 
 const XAxis = () => <Styles.DashedLine />
 
-const Axes: React.FC<{}> = props => (
-  <Styles.Root>
-    <YTick degrees={4} />
-    <YTick degrees={3} />
-    <YTick degrees={2} />
-    <YTick degrees={1} />
-    <XAxis />
-    <YTick degrees={-1} />
-    <YTick degrees={-2} />
-    <YTick degrees={-3} />
-    <YTick degrees={-4} />
-  </Styles.Root>
-)
+const Axes: React.FC<{}> = props => {
+  const [ref, percentage] = useScrollPercentage()
+  const [yTicksVisible, setYTicksVisible] = useState(false)
+
+  useEffect(() => {
+    console.log(percentage)
+    if (percentage >= 0.5 && !yTicksVisible) {
+      setYTicksVisible(true)
+    } else if (percentage < 0.5 && yTicksVisible) {
+      setYTicksVisible(false)
+    }
+  }, [percentage])
+
+  return (
+    <Styles.Root ref={ref}>
+      {yTicksVisible && (
+        <>
+          <YTick degrees={4} />
+          <YTick degrees={3} />
+          <YTick degrees={2} />
+          <YTick degrees={1} />
+        </>
+      )}
+      <XAxis />
+      {yTicksVisible && (
+        <>
+          <YTick degrees={-1} />
+          <YTick degrees={-2} />
+          <YTick degrees={-3} />
+          <YTick degrees={-4} />
+        </>
+      )}
+    </Styles.Root>
+  )
+}
 
 export default Axes
--- src/components/TemperatureOverTime/Axes/styles.ts
+++ src/components/TemperatureOverTime/Axes/styles.ts
@@ -1,10 +1,10 @@
-import styled from 'styled-components'
+import styled, { keyframes, css } from 'styled-components'
 
 export const Root = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  justify-content: space-between;
+  justify-content: space-evenly;
 
   height: 100vh;
 `
@@ -16,4 +16,35 @@ export const DegreesNumber = styled.span`
 export const DashedLine = styled.div`
   border-bottom: 2px dashed black;
   width: 100%;
+`
+
+export const Dash = styled.span`
+  color: white;
+`
+
+const yTickAnimation = keyframes`
+from {
+  opacity: 0;
+  transform: translateX(5px);
+}
+
+to {
+  opacity: 1;
+  transform: translateX(0);
+}
+`
+
+const animationDelays = Array(8)
+  .fill(null)
+  .map(
+    (_, index) => css`
+      &:nth-of-type(${index + 1}) {
+        animation-delay: ${index * 0.05}s;
+      }
+    `
+  )
+
+export const YTick = styled.span`
+  animation: ${yTickAnimation} 0.5s backwards ease-in-out;
+  ${animationDelays}
 `
