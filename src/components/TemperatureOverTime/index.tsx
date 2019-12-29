508ff46ba0d831bab6189e2acda92ab9e0dc7b93
Jesse Pinho <jesse@jessepinho.com>
2019-12-29 13:49:29 +0100

    feat: build out more content for the TemperatureOverTime

--- src/components/TemperatureOverTime/index.tsx
+++ src/components/TemperatureOverTime/index.tsx
@@ -7,15 +7,20 @@ import * as Styles from './styles'
 const TemperatureOverTime: React.FC<{}> = props => (
   <Styles.Section>
     <Styles.H2>How do we know it's real?</Styles.H2>
-    <Styles.P>
+    <Styles.IntroText>
       Climate is constantly changing. That’s what it has always done and what it
       will keep on doing. What is happening now though is different from the
       natural fluctuation before.
-    </Styles.P>
+    </Styles.IntroText>
 
     <Sticky>
       <Axes />
     </Sticky>
+
+    <Styles.P>Let’s look at some benchmarks of the earth’s climate.</Styles.P>
+    <Styles.P>
+      This is the end of ice age. It was about 4.3°C colder than today.
+    </Styles.P>
   </Styles.Section>
 )
 
--- src/components/TemperatureOverTime/styles.ts
+++ src/components/TemperatureOverTime/styles.ts
@@ -15,8 +15,19 @@ export const H2 = styled.h2`
   padding: 2.5rem 1rem 0;
 `
 
-export const P = styled.p`
+export const IntroText = styled.p`
   font-size: 1.25rem;
   line-height: 1.875rem;
   padding: 0 1rem;
+`
+
+export const P = styled.p`
+  width: 8rem;
+  line-height: 1.25rem;
+  padding-left: 1rem;
+  margin-bottom: 75vh;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
 `
