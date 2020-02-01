c8134bbf85c18fd6879a4835b25a0c6c856314a7
Jesse Pinho <jesse@jessepinho.com>
2020-02-01 20:56:22 +0100

    fix: use color: inherit for links

--- src/components/MainCSS.ts
+++ src/components/MainCSS.ts
@@ -3,6 +3,10 @@ import { createGlobalStyle } from 'styled-components'
 import { FontFamily } from '../lib/theme'
 
 const MainCSS = createGlobalStyle`
+  a {
+    color: inherit;
+  }
+
   body {
     font-family: ${FontFamily.Primary};
   }
